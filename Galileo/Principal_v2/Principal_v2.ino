55941dfaec63249e5549ceca0a081d2991a56388
DKingyo <fethallah.bentoumi@gmail.com>
2015-10-21 19:15:06 +0200

    Ajout des commentaires & autres
    Ajout des commentaires
    Suppression des messages de tests (comme le parse du json)
    Ajout du menu par incrément d'une variable (fonctionne, mais à cause du
    temps sur la fonction "monitor()", prend du temps à prendre en compte
    l'appuie du bouton.

--- Galileo/Principal_v2/Principal_v2.ino
+++ Galileo/Principal_v2/Principal_v2.ino
@@ -23,22 +23,23 @@ struct comDatas{
 
 struct comDatas tmp;
 
-int lcd_key       = 0;
-int adc_key_in    = 0;
-int adc_key_prev  = 0;
+int lcd_key       =  0;
+int adc_key_in    =  0;
+int adc_key_prev  =  0;
+int incr          =  0;
 
 byte mac[] = { 0x98, 0x4F, 0xEE, 0x05, 0x34, 0x15 };
 IPAddress server(192,168,2,1);
-char* serverAddress = "192.168.2.1";
-int port = 80;
-String message = "";
+char* serverAddress   =   "192.168.2.1";
+int port              =   80;
+String message        =   "";
 
 EthernetClient client;
 WebSocketClient client2;
 
 void setup() {
-  tmp.busy = "False";
-  Serial.begin(9600);
+  tmp.busy = "False";         /* On part du principe que le cab est libre de base */
+  Serial.begin(9600);         
   lcd.begin(16,2);            /* initialisation du lcd */
   lcd.setCursor(0,0);
   lcd.print("   En attente   ");
@@ -78,67 +79,98 @@ void setup() {
 }
 
 void loop() {
-  client2.monitor();             
+  client2.monitor();  /* Exécute le onOpen, onMessage et onError de sa classe */
+  
+  IPAddress myAddr = Ethernet.localIP(); /* Stocke l'adresse IP donné par le serveur (pour la traiter après) */
   adc_key_prev = lcd_key ;       // Looking for changes
   lcd_key = read_LCD_buttons();  // read the buttons
 
-  lcd.clear();
-  if(tmp.busy == "False"){
-    lcd.print("Free");
+  if(incr==0){  /* Affiche les informations relatives aux trames reçues */
+   lcd.clear();
+   if(tmp.busy == "False"){
+     lcd.print("Free");
+   }
+   else{
+     lcd.print("Busy");
+   }
+   
+   lcd.setCursor((12-(sizeof(tmp.id)/8)),0);
+   lcd.print("id:");
+   lcd.setCursor((15-(sizeof(tmp.id)/8)),0);
+   lcd.print(tmp.id);
+   lcd.setCursor(0,1);
+   lcd.print("Queue:");
+   lcd.print(tmp.queue);
+   lcd.print("  km:");
+   lcd.print(tmp.km);
   }
-  else{
-    lcd.print("Busy");
+  else if(incr==1){ /* Menu2: Affiche l'IP donné au Galileo par le serveur */
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Mon IP HiCab :");
+    lcd.setCursor(0,1);
+    lcd.print(myAddr[0]);
+    lcd.print(".");
+    lcd.print(myAddr[1]);
+    lcd.print(".");
+    lcd.print(myAddr[2]);
+    lcd.print(".");
+    lcd.print(myAddr[3]);
   }
-  
-  lcd.setCursor((12-(sizeof(tmp.id)/8)),0);
-  lcd.print("id:");
-  lcd.setCursor((14-(sizeof(tmp.id)/8)),0);
-  lcd.print(tmp.id);
-  lcd.setCursor(0,1);
-  lcd.print("Queue:");
-  lcd.print(tmp.queue);
-  lcd.print(" - km:");
-  lcd.print(tmp.km);
-  
+  else if(incr==2){ /* Why not */
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Version 1.2.34.2");
+    lcd.setCursor(0,1);
+    lcd.print("ISO 9000 ");
+  }
   switch (lcd_key)               // depending on which button was pushed, we perform an action
   {
-    case btnRIGHT:
-    {
+    case btnRIGHT:  /* Envoie l'acceptation de la première trame de la queue */
+    {               /* Dans le cas où le cab n'est pas occupé et que la queue n'est pas vide */
       if(tmp.busy=="False" && tmp.queue!="0"){
         char *publishMessageC = "{\"is_accepted\":true}";
         client2.send(publishMessageC);
       }
       break;
     }
-    case btnLEFT:
-    {
+    case btnLEFT:   /* Envoie le refus de la première trame de la queue */
+    {               /* Dans le cas où le cas n'est pas occupé et que la queue n'est pas vide */
       if(tmp.busy=="False" && tmp.queue!="0"){
         char *publishMessageC = "{\"is_accepted\":false}";
         client2.send(publishMessageC);
       }
       break;
     }
-    case btnUP:
+    case btnUP: /* Bouton Haut et Bas pour switcher entre les menus */
     {
-
+      incr++;
+      Serial.println("UP");
+      if(incr==3)
+        incr=0;
       break;
     }
     case btnDOWN:
     {
-
+      incr--;
+      Serial.println("DOWN");
+      if(incr<0)
+        incr=2;
       break;
     }
     case btnSELECT:
     {
       break;
     }
-    case btnNONE:
+    case btnNONE: 
     {
       break;
     }
   }
 }
 
+/* Fonction pour lire l'état des boutons
+Code repris, mais modification des valeurs */
 int read_LCD_buttons(){
   adc_key_in = analogRead(0);      // read the value from the sensor 
   delay(5); //switch debounce delay. Increase this delay if incorrect switch selections are returned.
@@ -148,10 +180,11 @@ int read_LCD_buttons(){
   if (adc_key_in < 220)  return btnUP; 
   if (adc_key_in < 420)  return btnDOWN; 
   if (adc_key_in < 640)  return btnLEFT; 
-  if (adc_key_in < 790)  return btnSELECT;   
+  if (adc_key_in < 790)  return btnSELECT;   /* N'est pas implémenté (et ne marche pas) */
   return btnNONE;  
 }
 
+/* A l'ouverture de la websocket, envoie une trame json concernant l'identifiant */
 void onOpen(WebSocketClient client){
   Serial.println("onOpen");
   String idtmpS = "{\"id_cab\": ";
@@ -163,32 +196,21 @@ void onOpen(WebSocketClient client){
   Serial.println(idtmpC);
 }
 
+/* Récupère les messages */
 void onMessage(WebSocketClient client, char* message) {
   Serial.println("onMessage");
   Serial.println(String(message));
   tmp = ComProcess(message,tmp);
-  Serial.print("odometer: ");
-  Serial.print(tmp.km);
-  Serial.println(".");
-  Serial.print("id_cab: ");
-  Serial.print(tmp.id);
-  Serial.println(".");
-  Serial.print("is_busy: ");
-  Serial.print(tmp.busy);
-  Serial.println(".");
-  Serial.print("Queue : ");
-  Serial.print(tmp.queue);
-  Serial.println(".");
- 
-  Serial.print("Received: "); Serial.println(message);
 }
 
+/* Récupère les erreurs */
 void onError(WebSocketClient client, char* message) {
   Serial.println("onError");
   Serial.print("ERROR: "); Serial.println(message);
 }
 
-struct comDatas ComProcess(String in,struct comDatas Datas){ //Pour parser le string
+/* Fonction de parse */
+struct comDatas ComProcess(String in,struct comDatas Datas){ 
     //struct comDatas Datas;
     
     if(in.indexOf("\'odometer\':") != -1){ 
